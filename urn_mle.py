a7cb49f406123a0b3e72a8884d65ccdd39126837
Craig L Brooks <feanorian@users.noreply.github.com>
2023-04-09 21:17:02 -0500

    Update urn_mle.py

--- urn_mle.py
+++ urn_mle.py
@@ -1,3 +1,14 @@
+"""
+Name: Craig Brooks
+PHSX 815 Spring 2023
+Project # 13
+Due Date 4/10/2023
+This code generates samples from 3 urns with White and Black marbles where the user defines the number sample for each trial and the number of trials. 
+This data is written to a file and which another script, 'urn_mle_ana;ysis' will determine the maximum likelihood estimate (MLE) for p = the probability 
+of drawing a White/Black marble
+"""
+
+
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
